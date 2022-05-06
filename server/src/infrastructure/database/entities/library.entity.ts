37def96718a032e62585012f7a127f9340d47a7d
mrs-eload <elodie.philippe90@gmail.com>
2022-05-06 03:29:29 +0200

    Init project

--- server/src/infrastructure/database/entities/library.entity.ts
+++ server/src/infrastructure/database/entities/library.entity.ts
@@ -1,12 +1,10 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Library extends BaseEntity {
-
   @PrimaryColumn()
   id: number;
 
   @Column()
   name: string;
-
 }
